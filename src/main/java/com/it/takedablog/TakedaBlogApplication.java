ef791b4bbeb3fb742b32b9cd120885addeb6ea83
Pham Minh Luan <minhluanpham1309@gmail.com>
2024-04-18 10:41:44 +0700

    add new backend

---
+++ src/main/java/com/it/takedablog/TakedaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.it.takedablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakedaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakedaBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/it/takedablog/TakedaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.takedablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakedaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

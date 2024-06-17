d3d959439bc6ea71841f8b6e9779d9a1d4e5cd37
Minh Luan <phamminhluan@fabercompany.co.jp>
2024-06-17 17:33:50 +0700

    test

---
+++ js/heatmap-popup-embed.js
@@ -0,0 +1,14 @@
+let a = document.createElement('script');
+a.type = 'text/javascript';
+a.async = true;
+a.src = 'http://localhost:8085//embed'
+    + '?service=heatmap-popup&tokenId='
+    + "281543725"
+    + '&protocol=' + window.location.protocol
+    + '&hostname=' + window.location.hostname
+    + '&pathname=' + encodeURIComponent(window.location.pathname)
+    + '&search=' + encodeURIComponent(window.location.search)
+    + '&hash=' + encodeURIComponent(window.location.hash)
+    + '&dv=' + "d";
+let b = document.getElementsByTagName('script')[0];
+b.parentNode.insertBefore(a, b);

32d14f26e925959610553fa68fc5baec13f58f88
Minh Luan <phamminhluan@fabercompany.co.jp>
2024-07-31 11:29:22 +0700

    no message

--- js/test.js
+++ js/test.js
@@ -18,15 +18,14 @@ var MierucaHM = function () {
         text: "",
         ipa: 0,
         device: (function () {
-            var fullPattern = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i,
-                    shortPattern = /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i,
-                    tabletPattern = /android|ipad|playbook|silk/i,
-                    userAgent = navigator.userAgent || navigator.vendor || window.opera;
-            if (fullPattern.test(userAgent) || shortPattern.test(userAgent.substr(0, 4))) {
-                return "m";
-            }
+            var mobilePattern = /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/,
+                tabletPattern = /(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i,
+                userAgent = navigator.userAgent || navigator.vendor || window.opera;
             if (tabletPattern.test(userAgent)) {
                 return "t";
+            }
+            if (mobilePattern.test(userAgent)) {
+                return "m";
             }
             return "d";
         }())},
@@ -64,7 +63,6 @@ var MierucaHM = function () {
 
                     //<editor-fold defaultstate="collapsed" desc="ON OPEN">
                     webSocket.onopen = function () {
-                        (function () {
                             var pageView = {};
                             pageView["type"] = 'p';
                             pageView["sId"] = hmObj.site_id;
@@ -78,7 +76,6 @@ var MierucaHM = function () {
                             pageView["ipa"] = "ipa";
                             pageView["ua"] = navigator.userAgent || navigator.vendor || window.opera;
                             webSocket.sendMessage(JSON.stringify(pageView));
-                        })();
                     };
                     //</editor-fold>
 
@@ -103,25 +100,36 @@ var MierucaHM = function () {
                         // Just Show popup when site has not reached limit pv
                         if (event.data === "IS_ALLOW_LOAD_POPUP") {
                             hm.popupHandle();
+                            return;
+                        }
+
+                        if (event.data.startsWith("refId-")) {
+                            window.__hmrid = event.data.split("-")[1];
+                            return;
+                        }
+
+                        if (event.data.startsWith("urlId-")) {
+                            window.__hmuid = event.data.split("-")[1];
+                            return;
                         }
                     };
                     //</editor-fold>
 
                     //Click handle
-                    var elements = document.querySelectorAll("label,a,input,button,textarea,img,iframe,video"), i = 0;
-                    for (i; i < elements.length; i += 1) {
-                        hm.setEventClickListener(elements[i]);
-                    }
-
-                    //Scroll handle
-                    // window.addEventListener("scroll", function () {
-                    //     if (hmObj.time_out) {
-                    //         return;
-                    //     }
-                    //     hm.scrollHandle();
-                    //     hm.readHandle();
-                    //     hm.resetTimeOut();
-                    // });
+//                    var elements = document.querySelectorAll("label,a,input,button,textarea,img,iframe,video"), i = 0;
+//                    for (i; i < elements.length; i += 1) {
+//                        hm.setEventClickListener(elements[i]);
+//                    }
+//
+//                    //Scroll handle
+//                    window.addEventListener("scroll", function () {
+//                        if (hmObj.time_out) {
+//                            return;
+//                        }
+//                        hm.scrollHandle();
+//                        hm.readHandle();
+//                        hm.resetTimeOut();
+//                    });
                     hm.resetTimeOut();
 
 
@@ -186,12 +194,12 @@ var MierucaHM = function () {
                         hm.initWindowElement();
                         if (!hmObj.time_out) {
                             e = e || window.event;
-                            var ampl = 50, 
+                            var ampl = 50,
                                 xP = e.pageX !== undefined ? Math.floor(e.pageX) : 0,
                                 yP = e.pageY !== undefined ? Math.floor(e.pageY) : 0,
-                                isNotPinchedActionX = Math.abs(hmObj.tempXp - xP) < ampl,
-                                isNotPinchedActionY = Math.abs(hmObj.tempYp - yP) < ampl;
-                            if ((hmObj.device === "m") && (isNotPinchedActionX && isNotPinchedActionY)) {
+                                isPinchedActionX = Math.abs(hmObj.tempXp - xP) < ampl,
+                                isPinchedActionY = Math.abs(hmObj.tempYp - yP) < ampl;
+                            if ((hmObj.device === "m") && (isPinchedActionX && isPinchedActionY)) {
                                 if (this.nodeName !== "A") {
                                     return;
                                 }
@@ -227,28 +235,29 @@ var MierucaHM = function () {
                 },
                 //</editor-fold>
                 //<editor-fold defaultstate="collapsed" desc="HANDLE LOAD POPUP SCRIPT EVENT">
-                // popupHandle: function() {
-                //     let a = document.createElement('script');
-                //     a.type = 'text/javascript';
-                //     a.async = true;
-                //     a.src = document.location.protocol + '//hpjp.mieru-ca.com/embed'
-                //             + '?service=heatmap-popup&tokenId='
-                //             + window.__fid[0][0]
-                //             + '&protocol=' + window.location.protocol
-                //             + '&hostname=' + window.location.hostname
-                //             + '&pathname=' + encodeURIComponent(window.location.pathname)
-                //             + '&search=' + encodeURIComponent(window.location.search)
-                //             + '&hash=' + encodeURIComponent(window.location.hash)
-                //             + '&dv=' + hmObj.device;
-                //     let b = document.getElementsByTagName('script')[0];
-                //     b.parentNode.insertBefore(a, b);
-                // }
+//                popupHandle: function() {
+//                    let a = document.createElement('script');
+//                    a.type = 'text/javascript';
+//                    a.async = true;
+//                    a.src = document.location.protocol + '//hpjp.mieru-ca.com/embed'
+//                            + '?service=heatmap-popup&tokenId='
+//                            + window.__fid[0][0]
+//                            + '&protocol=' + window.location.protocol
+//                            + '&hostname=' + window.location.hostname
+//                            + '&pathname=' + encodeURIComponent(window.location.pathname)
+//                            + '&search=' + encodeURIComponent(window.location.search)
+//                            + '&hash=' + encodeURIComponent(window.location.hash)
+//                            + '&dv=' + hmObj.device;
+//                    let b = document.getElementsByTagName('script')[0];
+//                    b.parentNode.insertBefore(a, b);
+//                }
                 //</editor-fold>
             };
 
     //<editor-fold defaultstate="collapsed" desc="INIT">
     this.init = function () {
-        hm.sendPageView();
+        if (userAgent.includes("Lighthouse")) {return;}
+        setTimeout(hm.sendPageView(), 500);
     };
     //</editor-fold>
 };
